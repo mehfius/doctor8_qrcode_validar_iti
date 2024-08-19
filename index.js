b0b6505aa71d022a709463c8402286e4b3fa9f68
matheus ferraz <matheus.ferraz@gmail.com>
2024-08-18 21:12:50 -0300

    .

--- index.js
+++ index.js
@@ -3,35 +3,57 @@ const cors = require('cors');
 const corsMiddleware = cors({ origin: true });
 const { createClient } = require("@supabase/supabase-js");
 
-functions.http('qrcode_validar_iti', async (req, res) => {
-    corsMiddleware(req, res, async () => {
-        
-        if (!req.query.id) return { error: 'Parâmetro id vazio', location: arguments.callee.name };
+const handleRequest = async (req, res) => {
+  const { body } = req;
 
-        const supabase = createClient(process.env.URL, process.env.KEY);
+  try {
+
     
-        const { data, error } = await supabase
-        .from('files')
-        .select('filename')
-        .eq('id', req.query.id);
-        
-        console.log(error)
+    //if (!body.id) return { error: 'Parâmetro id vazio', location: arguments.callee.name };
 
-        let filename = data[0]['filename']
+    const supabase = createClient(process.env.URL, process.env.KEY);
 
-        const response = {
-          version: "1.0.0",
-          prescription: {
-            signatureFiles: [
-              {
-                url:'https://vflhuqqzjmgkdhjgxzni.supabase.co/storage/v1/object/public/pdf/'+filename+'/48ae19b2-a922-4407-9a6d-d168cb412072.pdf'
-              }
-            ]
+    const { data, error } = await supabase
+    .from('files')
+    .select('filename,files')
+    .eq('id', body.data.id);
+    
+    console.log(error)
+
+    let filename = data[0]['filename']
+    let files = data[0]['files']
+  
+    const response = {
+      version: "1.0.0",
+      prescription: {
+        signatureFiles: [
+          {
+            url:'https://vflhuqqzjmgkdhjgxzni.supabase.co/storage/v1/object/public/pdf/'+files+'/'+filename
           }
-        };
-      //          url: `https://vflhuqqzjmgkdhjgxzni.supabase.co/storage/v1/object/public/pdf/${files}/${filename}`
-        res.set('Content-Type', 'application/json');
-        res.send(JSON.stringify(response));
+        ]
+      }
+    };
 
-    });
+    res.set('Content-Type', 'application/json');
+   
+    return res.status(200).send(JSON.stringify(response));
+
+  /*   switch (body.path) {
+      case 'listPSC':
+        const psc = await routeListPSC(body.session);
+        return res.status(200).send(psc);
+      case 'updateUsersPSC':
+        const update = await routeUpdateUsersPSC(body.session);
+        return res.status(200).send(update);
+      default:
+        return res.status(400).send({ error: 'Invalid path' });
+    } */
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).send('Error');
+  }
+};
+
+functions.http('qrcode_validar_iti', (req, res) => {
+  corsMiddleware(req, res, () => handleRequest(req, res));
 });
